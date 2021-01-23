ef9b21d1a6317752074fec93bc5c461d3df24fc1
Luciano <lucianomammino@gmail.com>
2021-01-23 19:53:03 +0000

    Added example using only promises

--- README.md
+++ README.md
@@ -6,7 +6,8 @@ Check out the code in the [`examples`](./examples) folder.
 
  - [`examples/race-dummy.js`](./examples/race-dummy.js): explores a simple race condition.
  - [`examples/race-simple-fix.js`](./examples/race-simple-fix.js): provides a simple solution using `async/await`.
- - [`examples/race-mutex.js`](./examples/race-mutex.js): provides a solution using mutexes.
+ - [`examples/race-mutex.js`](./examples/race-mutex.js): provides a solution using mutexes (using the library [`async-mutex`](https://npm.im/async-mutex)).
+ - [`examples/race-mutex-no-lib.js`](./examples/race-mutex-no-lib.js): provides a solution using mutexes (implementing the mutex pattern by ourselves).
 
 These examples are illustrated in the article: [Node.js race conditions](https://www.nodejsdesignpatterns.com/blog/node-js-race-conditions).
 
---
+++ examples/race-mutex-no-lib.js
@@ -0,0 +1,61 @@
+// Utility function to simulate some delay (e.g. reading from or writing to a database).
+// It will take from 0 to 50ms in a random fashion.
+const randomDelay = () => new Promise(resolve =>
+  setTimeout(resolve, Math.random() * 100)
+)
+
+// Our global balance.
+// In a more complete implementation, this will live in the persistent data storage.
+let balance = 0
+let mutex = Promise.resolve() // global mutex instance
+
+async function loadBalance () {
+  // simulates random delay to retrieve data from data storage
+  await randomDelay()
+  return balance
+}
+
+async function saveBalance (value) {
+  // simulates random delay to write the data to the data storage
+  await randomDelay()
+  balance = value
+}
+
+async function sellGrapes () {
+  mutex = mutex.then(async () => {
+    const balance = await loadBalance()
+    console.log(`sellGrapes - balance loaded: ${balance}`)
+    const newBalance = balance + 50
+    await saveBalance(newBalance)
+    console.log(`sellGrapes - balance updated: ${newBalance}`)
+  })
+  mutex.catch(() => {})
+  return mutex
+}
+
+async function sellOlives () {
+  mutex = mutex.then(async () => {
+    const balance = await loadBalance()
+    console.log(`sellOlives - balance loaded: ${balance}`)
+    const newBalance = balance + 50
+    await saveBalance(newBalance)
+    console.log(`sellOlives - balance updated: ${newBalance}`)
+  })
+  mutex.catch(() => {})
+  return mutex
+}
+
+async function main () {
+  await Promise.all([
+    sellGrapes(),
+    sellOlives(),
+    sellGrapes(),
+    sellOlives(),
+    sellGrapes(),
+    sellOlives()
+  ])
+  const balance = await loadBalance()
+  console.log(`Final balance: ${balance}`)
+}
+
+main()
