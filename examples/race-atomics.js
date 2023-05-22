d26bcb904e729e589f600553c969d1c716f6b2f1
tyson <tyson.midboe@gmail.com>
2023-05-22 01:33:53 -0500

    Update race-atomics.js

--- examples/race-atomics.js
+++ examples/race-atomics.js
@@ -1,79 +1,8 @@
-
-function test1 () {
-
+function showRaceCondition () {
   // Utility function to simulate some delay (e.g. reading from or writing to a database).
   // It will take from 0 to 50ms in a random fashion.
-  const randomDelay = () => new Promise(resolve =>
-    setTimeout(resolve, Math.random() * 100)
-  )
-
-  // Our global balance.
-  // In a more complete implementation, this will live in the persistent data storage.
-  const BALANCE_INDEX = 0
-  const balanceStorage = new BigInt64Array(1)
-
-  async function loadBalance () {
-    // simulates random delay to retrieve data from data storage
-    await randomDelay()
-    return Atomics.load(balanceStorage, BALANCE_INDEX)
-  }
-
-  async function saveBalance (value) {
-    // simulates random delay to write the data to the data storage
-    await randomDelay()
-    Atomics.add(balanceStorage, BALANCE_INDEX, BigInt(value))
-  }
-
-  async function sellGrapes () {
-    try {
-      console.log(`sellGrapes - balance loaded: ${await loadBalance()}`)
-      await saveBalance(50)
-      console.log(`sellGrapes - balance`)
-      console.log(`sellOlives - balance loaded: ${await loadBalance()}`)
-      // const newBalance = balance + 50
-      await saveBalance(50)
-      console.log(`sellOlives - balance updated: ${await loadBalance()}`)
-    } finally {
-      // release()
-    }
-  }
-
-  async function sellOlives () {
-    try {
-      console.log(`sellGrapes - balance loaded: ${await loadBalance()}`)
-      await saveBalance(50)
-      console.log(`sellGrapes - balance`)
-      console.log(`sellOlives - balance loaded: ${await loadBalance()}`)
-      // const newBalance = balance + 50
-      await saveBalance(50)
-      console.log(`sellOlives - balance updated: ${await loadBalance()}`)
-    } finally {
-      //    release()
-    }
-  }
-
-  async function main () {
-    await Promise.all([
-      sellGrapes(),
-      sellOlives(),
-      sellGrapes(),
-      sellOlives(),
-      sellGrapes(),
-      sellOlives()
-    ])
-    const balance = await loadBalance()
-    console.log(`Final balance: ${balance}`)
-  }
-
-  main()
-}
-
-function test2 () {
-  // Utility function to simulate some delay (e.g. reading from or writing to a database).
-  // It will take from 0 to 50ms in a random fashion.
-  const randomDelay = () => new Promise(resolve =>
-    setTimeout(resolve, Math.random() * 100)
-  )
+  const randomDelay = () =>
+    new Promise(resolve => setTimeout(resolve, Math.random() * 100))
 
   // Our global balance.
   // In a more complete implementation, this will live in the persistent data storage.
@@ -108,8 +37,11 @@ function test2 () {
   }
 
   async function main () {
-    await sellGrapes()
-    await sellOlives()
+    const transaction1 = sellGrapes() // NOTE: no `await`
+    const transaction2 = sellOlives() // NOTE: no `await`
+    await transaction1 // NOTE: awaiting here does not stop `transaction2`
+    // from being scheduled before transaction 1 is completed
+    await transaction2
     const balance = await loadBalance()
     console.log(`Final balance: ${balance}`)
   }
@@ -117,32 +49,33 @@ function test2 () {
   main()
 }
 
-function test3 () {
+function fixRaceCondition () {
   // Utility function to simulate some delay (e.g. reading from or writing to a database).
   // It will take from 0 to 50ms in a random fashion.
-  const randomDelay = () => new Promise(resolve =>
-    setTimeout(resolve, Math.random() * 100)
-  )
+  const randomDelay = () =>
+    new Promise(resolve => setTimeout(resolve, Math.random() * 100))
 
   // Our global balance.
   // In a more complete implementation, this will live in the persistent data storage.
-  const balanceStorage = new BigInt64Array(1)
+  const balance = new BigInt64Array(1)
   const BALANCE_INDEX = 0
 
   async function loadBalance () {
     // simulates random delay to retrieve data from data storage
     await randomDelay()
-    return Atomics.load(balanceStorage, BALANCE_INDEX)
+    return Atomics.load(balance, BALANCE_INDEX)
   }
 
   async function saveBalance (value) {
     // simulates random delay to write the data to the data storage
     await randomDelay()
-    Atomics.add(balanceStorage, BALANCE_INDEX, BigInt(value))
+    Atomics.add(balance, BALANCE_INDEX, BigInt(value))
   }
 
   async function sellGrapes () {
-    console.log(`sellGrapes - balance loaded: ${await loadBalance()}`)
+    const balance = await loadBalance()
+    console.log(`sellGrapes - balance loaded: ${balance}`)
+    // const newBalance = balance + 50
     await saveBalance(50)
     console.log(`sellGrapes - balance updated: ${await loadBalance()}`)
   }
@@ -150,17 +83,32 @@ function test3 () {
   async function sellOlives () {
     const balance = await loadBalance()
     console.log(`sellOlives - balance loaded: ${balance}`)
+    // const newBalance = balance + 50
     await saveBalance(50)
     console.log(`sellOlives - balance updated: ${await loadBalance()}`)
   }
 
   async function main () {
-    await sellGrapes()
-    await sellOlives()
-    console.log(`Final balance: ${await loadBalance()} `)
+    const transaction1 = sellGrapes() // NOTE: no `await`
+    const transaction2 = sellOlives() // NOTE: no `await`
+    await transaction1 // NOTE: awaiting here does not stop `transaction2`
+    // from being scheduled before transaction 1 is completed
+    await transaction2
+    const balance = await loadBalance()
+    console.log(`Final balance: ${balance}`)
   }
 
   main()
 }
 
-test3()
+for (let i = 0; i < 5; i++) {
+  showRaceCondition()
+}
+
+setTimeout(() => {
+  console.log('\natomics fix')
+  console.log('-------------')
+  for (let i = 0; i < 5; i++) {
+    fixRaceCondition()
+  }
+}, 1500)
